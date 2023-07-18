12203bf5704a81be21a536ba418ca5a51788f37e
Aakash Kc <aakashkcx@gmail.com>
2023-07-18 01:42:52 +0100

    fix syntax

--- src/search.ts
+++ src/search.ts
@@ -17,24 +17,23 @@ export class Search {
   }
 
   search(depth: number = 3): Move {
-
     this.alphaBeta(depth, -Infinity, Infinity);
 
     return this.bestMove;
   }
 
-  alphaBeta(depth: number, alpha: number; beta: number): number {
-    const {game} = this;
+  alphaBeta(depth: number, alpha: number, beta: number): number {
+    const { game } = this;
 
     if (depth === 0) return evaluate(game);
 
     this.nodes++;
 
     const moves = generateMoves(game);
-    for (const move of moves){
-      game.makeMove(move)
-      game.takeBack()
-    }    
+    for (const move of moves) {
+      game.makeMove(move);
+      game.takeBack();
+    }
 
     return this.bestScore;
   }
