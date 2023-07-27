b8b1783706e3201c9034fa6e9b3855cda9b3e3a5
Aakash Kc <aakashkcx@gmail.com>
2023-07-27 17:19:23 +0100

    add mate check

--- demo/search.ts
+++ demo/search.ts
@@ -1,4 +1,4 @@
-import { ChessGame } from "../src";
+import { ChessGame, NO_MOVE } from "../src";
 
 const FEN =
   "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
@@ -7,9 +7,13 @@ game.print();
 
 while (true) {
   const move = game.search(3000);
-  if (move) {
+  if (move !== NO_MOVE) {
     game.makeMove(move);
     game.print();
+  } else {
+    if (game.inCheck) console.log("Checkmate!");
+    else console.log("Stalemate!");
+    break;
   }
 }
 
